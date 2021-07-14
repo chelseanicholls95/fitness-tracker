f1e29cee2f0d0752238a50dbf896d1c7f5b7dd33
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-07-14 20:33:54 +0100

    add workout controller wip

--- src/controllers/api/index.js
+++ src/controllers/api/index.js
@@ -9,9 +9,28 @@ const getAllWorkouts = async (req, res) => {
     return res.status(500).json({ error: "Failed to get workouts" });
   }
 };
-const addWorkout = (req, res) => {
-  console.log(req);
+const addWorkout = async (req, res) => {
+  try {
+    const { day, exercise } = req.body;
+
+    const workout = {
+      day,
+      exercise,
+    };
+
+    await Workout.create(workout);
+
+    if (res.status === 404) {
+      return res.json({ error: "Failed to add workout" });
+    }
+
+    return res.json({ success: "Workout successfully added" });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ error: "Failed to add workout" });
+  }
 };
+
 const updateWorkout = (req, res) => {
   res.send("update workout");
 };
